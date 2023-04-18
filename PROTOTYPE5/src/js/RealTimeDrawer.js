d8cf931414b618135acd9a2ed45ac333171992f3
oopsuwu <60373320+oopsuwu@users.noreply.github.com>
2023-04-18 14:20:54 -0400

    3 colors

--- PROTOTYPE5/src/js/RealTimeDrawer.js
+++ PROTOTYPE5/src/js/RealTimeDrawer.js
@@ -20,8 +20,7 @@ RealTimeDrawer.prototype.setUpInteraction = function () {
 
 // Integer : Color mapping
 function ViewColorMap(num) {
-    return [{ "id": 1, "label": "Red" }, { "id": 2, "label": "Green" }, { "id": 3, "label": "Blue" },
-    { "id": 4, "label": "Yellow" }, { "id": 5, "label": "Cyan" }].find(x => x.id == num).label
+    return [{ "id": 1, "label": "Red" }, { "id": 2, "label": "Green" }, { "id": 3, "label": "Blue" }].find(x => x.id == num).label
 }
 
 RealTimeDrawer.prototype.setupColor = function () {
